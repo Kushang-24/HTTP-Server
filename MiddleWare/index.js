beec83674c2880e5e7120ca5a70b16b1d269afc3
Kushang-24 <122721466+Kushang-24@users.noreply.github.com>
2024-05-09 11:17:03 +0530

    commit

--- MiddleWare/index.js
+++ MiddleWare/index.js
@@ -17,7 +17,7 @@ const middleware = (req, res, next) => {
 // app.use(middleware);
 
 app.get('/', (req, res) => {
-    return res.render('index')
+    return res.render('layout')
 })
 
 app.get('/form-basic', (req, res) => {
@@ -58,3 +58,42 @@ app.listen(PORT, (err) => {
     }
     console.log("server started")
 })
+
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+// const PORT = 3000;
+// app.use(express.static(path.join(__dirname, 'public')));
+// app.set("view engine", "ejs");
+
+// const middleware = (req, res, next) => {
+//     console.log("hello");
+//     let age = req.query.age;
+//     if (age >= 18) {
+//         return next();
+//     }
+//     return res.redirect('/');
+// };
+
+// app.get('/', (req, res) => {
+//     return res.render('index');
+// });
+
+// app.get('/:page', (req, res) => {
+//     const page = req.params.page;
+//     const pages = ['form-basic', 'ui-buttons', 'ui-alerts', 'ui-card', 'ui-forms',
+//         'ui-typography', 'authentication-login', 'authentication-register', 'icon-tabler', 'sample-page'
+//     ];
+//     if (!pages.includes(page)) {
+//         return res.status(404).send('Page not found');
+//     }
+//     return res.render(page);
+// });
+
+// app.listen(PORT, (err) => {
+//     if (err) {
+//         console.log('Server not started');
+//         return false;
+//     }
+//     console.log("Server started");
+// });
